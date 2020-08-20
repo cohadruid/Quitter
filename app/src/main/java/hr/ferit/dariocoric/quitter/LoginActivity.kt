8982333ce7bdf7323ff6c044c3755a001369c7dd
Dario Coric <dariocrc11@gmail.com>
2020-08-20 21:01:44 +0200

    added google services

--- app/src/main/java/hr/ferit/dariocoric/quitter/LoginActivity.kt
+++ app/src/main/java/hr/ferit/dariocoric/quitter/LoginActivity.kt
@@ -1,2 +1,23 @@
 package hr.ferit.dariocoric.quitter
 
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class LoginActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+
+        text_view_dont_have_account.setOnClickListener {
+           /* val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)*/
+            finish()
+        }
+    }
+}
--- app/src/main/java/hr/ferit/dariocoric/quitter/MainActivity.kt
+++ app/src/main/java/hr/ferit/dariocoric/quitter/MainActivity.kt
@@ -1,12 +1,30 @@
 package hr.ferit.dariocoric.quitter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        button_register_main.setOnClickListener {
+            val email = edit_text_email_register.text.toString()
+            val password = edit_text_password_register.text.toString()
+
+            Log.d("MainActivity", "Email is $email")
+            Log.d("MainActivity", "Password is $password")
+        }
+
+        text_view_login_main.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

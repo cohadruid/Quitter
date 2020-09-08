d8241c23d49e972de10381cfeb85aba8198bcf37
Dario Coric <dariocrc11@gmail.com>
2020-09-08 16:39:35 +0200

    backup commit

--- app/src/main/java/hr/ferit/dariocoric/quitter/LoginActivity.kt
+++ app/src/main/java/hr/ferit/dariocoric/quitter/LoginActivity.kt
@@ -3,8 +3,11 @@ package hr.ferit.dariocoric.quitter
 import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,6 +16,27 @@ class LoginActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login)
+
+        button_login_login.setOnClickListener {
+            val email = edit_text_email_login.text.toString()
+            val password = edit_text_password_login.text.toString()
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if(!it.isSuccessful) {
+                        Log.d("LoginActivity","Attempted to login with e: $email and p: $password")
+                        Toast.makeText(this, "Check email/password", Toast.LENGTH_SHORT).show()
+                        return@addOnCompleteListener
+                    }
+
+                    Log.d("LoginActivity","Attempted to login with e: $email and p: $password")
+                    Log.d("LoginActivity", "UID: ${it.result?.user?.uid}")
+                    Toast.makeText(this, "good job", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener {
+                    Log.d("LoginActivity", "Failed to auth user - ${it.message}")
+                }
+        }
 
         text_view_dont_have_account.setOnClickListener {
            /* val intent = Intent(this, MainActivity::class.java)
--- app/src/main/java/hr/ferit/dariocoric/quitter/MainActivity.kt
+++ app/src/main/java/hr/ferit/dariocoric/quitter/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,19 +14,39 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
         button_register_main.setOnClickListener {
-            val email = edit_text_email_register.text.toString()
-            val password = edit_text_password_register.text.toString()
-
-            Log.d("MainActivity", "Email is $email")
-            Log.d("MainActivity", "Password is $password")
+            performRegister()
         }
 
         text_view_login_main.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun performRegister() {
+        val email = edit_text_email_register.text.toString()
+        val password = edit_text_password_register.text.toString()
+
+        if(email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter email/password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        //Log.d("MainActivity", "Email is $email")
+        //Log.d("MainActivity", "Password is $password")
+
+
+        //creating user with email and password
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if(!it.isSuccessful) return@addOnCompleteListener
+
+                Log.d("MainActivity", "Created user with uid ${it.result?.user?.uid}")
+            }
+            .addOnFailureListener {
+                Log.d("MainActivity", "Failed to create user: ${it.message}")
+                Toast.makeText(this, "Please enter credentials", Toast.LENGTH_SHORT).show()
+            }
     }
 }
